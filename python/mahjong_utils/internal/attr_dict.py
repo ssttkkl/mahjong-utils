ffc841c2b134e088aabf13165a0064005b5d0fa2
ssttkkl <huang.wen.long@hotmail.com>
2022-11-26 22:49:11 +0800

    删除python相关

--- python/mahjong_utils/internal/attr_dict.py
+++
@@ -1,6 +0,0 @@
-class attr_dict(dict):
-    def __getattr__(self, item):
-        return self.__getitem__(item)
-
-    def __setattr__(self, key, value):
-        return self.__setitem__(key, value)
