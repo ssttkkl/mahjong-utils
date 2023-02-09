f699e6d6e8fb340ab0cf784f67b0f4ecb2b37471
ssttkkl <huang.wen.long@hotmail.com>
2023-02-10 02:58:42 +0800

    Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="mahjong-utils",
-    version="0.3.2",
+    version="0.3.3",
     author="ssttkkl",
     author_email="huang.wen.long@hotmail.com",
     license="MIT",
