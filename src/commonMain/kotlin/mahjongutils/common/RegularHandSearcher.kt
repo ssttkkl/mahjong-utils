83b504594e13de1e702bde879dccc0cfe7f2a5a8
ssttkkl <huang.wen.long@hotmail.com>
2023-02-18 09:18:50 +0800

    优化手牌拆解算法

--- src/commonMain/kotlin/mahjongutils/common/RegularHandSearcher.kt
+++ src/commonMain/kotlin/mahjongutils/common/RegularHandSearcher.kt
@@ -14,20 +14,6 @@ private fun decode(code: Int): Tile {
     return Tile.get(type, num)
 }
 
-private inline fun generateKBitNumber(k: Int, callback: (Int) -> Boolean) {
-    var x = (1 shl k) - 1
-    while (callback(x)) {
-        // next k bit number
-        // REF: http://realtimecollisiondetection.net/blog/?p=78
-        val b = x and -x
-        val t = x + b
-        val c = x xor t
-        val m = (c shr 2) / b
-        val r = t or m
-        x = r
-    }
-}
-
 private class RegularHandPatternSearcher(
     tiles: List<Tile>,
     private val furo: List<Furo> = emptyList(),
@@ -38,6 +24,7 @@ private class RegularHandPatternSearcher(
     private val cnt = IntArray(3 * 9 + 7 + 1)
     private val mentsu = ArrayList<Mentsu>()
     private val tatsu = ArrayList<Tatsu>()
+    private var jyantou: Tile? = null
 
     init {
         tiles.forEach {
@@ -100,6 +87,24 @@ private class RegularHandPatternSearcher(
                 }
             }
         }
+        dfsJyantou()
+    }
+
+    private fun dfsJyantou(begin: Int = 0) {
+        if (n >= 2) {
+            for (i in begin until 3 * 9 + 7) {
+                val t = decode(i)
+                if (cnt[i] >= 2) {
+                    n -= 2
+                    cnt[i] -= 2
+                    jyantou = t
+                    dfsTatsu()
+                    n += 2
+                    cnt[i] += 2
+                    jyantou = null
+                }
+            }
+        }
         dfsTatsu()
     }
 
@@ -110,85 +115,89 @@ private class RegularHandPatternSearcher(
          * 故当for循环执行了一趟以后就将tatsu_type_limitation置0
          */
 
-        var tatsuTypeLimitation = tatsuTypeLimitation
         var taken = false
-        if (n >= 2) {
-            for (i in begin until 3 * 9 + 7) {
-                val t = decode(i)
 
-                // toitsu
-                if (tatsuTypeLimitation == 0 && cnt[i] >= 2) {
-                    taken = true
-                    n -= 2
-                    cnt[i] -= 2
-                    tatsu.add(Toitsu(t))
-                    dfsTatsu(i, 0)
-                    n += 2
-                    cnt[i] += 2
-                    tatsu.removeLast()
-                }
+        if (mentsu.size + tatsu.size < k) {
+            var tatsuTypeLimitation = tatsuTypeLimitation
+            if (n >= 2) {
+                for (i in begin until 3 * 9 + 7) {
+                    val t = decode(i)
 
-                // kanchan
-                if (tatsuTypeLimitation <= 1 && t.type != TileType.Z && t.num <= 7) {
-                    val j = i + 2
-                    if (cnt[i] > 0 && cnt[j] > 0) {
+                    // toitsu
+                    if (tatsuTypeLimitation == 0 && cnt[i] >= 2) {
                         taken = true
                         n -= 2
-                        cnt[i] -= 1
-                        cnt[j] -= 1
-                        tatsu.add(Kanchan(t))
-                        dfsTatsu(i, 1)
+                        cnt[i] -= 2
+                        tatsu.add(Toitsu(t))
+                        dfsTatsu(i, 0)
                         n += 2
-                        cnt[i] += 1
-                        cnt[j] += 1
+                        cnt[i] += 2
                         tatsu.removeLast()
                     }
-                }
 
-                // ryanmen
-                if (tatsuTypeLimitation <= 2 && t.type != TileType.Z && t.num in 2..7) {
-                    val j = i + 1
-                    if (cnt[i] > 0 && cnt[j] > 0) {
-                        taken = true
-                        n -= 2
-                        cnt[i] -= 1
-                        cnt[j] -= 1
-                        tatsu.add(Ryanmen(t))
-                        dfsTatsu(i, 2)
-                        n += 2
-                        cnt[i] += 1
-                        cnt[j] += 1
-                        tatsu.removeLast()
+                    // kanchan
+                    if (tatsuTypeLimitation <= 1 && t.type != TileType.Z && t.num <= 7) {
+                        val j = i + 2
+                        if (cnt[i] > 0 && cnt[j] > 0) {
+                            taken = true
+                            n -= 2
+                            cnt[i] -= 1
+                            cnt[j] -= 1
+                            tatsu.add(Kanchan(t))
+                            dfsTatsu(i, 1)
+                            n += 2
+                            cnt[i] += 1
+                            cnt[j] += 1
+                            tatsu.removeLast()
+                        }
                     }
-                }
 
-                // penchan
-                if (tatsuTypeLimitation <= 3 && t.type != TileType.Z && (t.num == 1 || t.num == 8)) {
-                    val j = i + 1
-                    if (cnt[i] > 0 && cnt[j] > 0) {
-                        taken = true
-                        n -= 2
-                        cnt[i] -= 1
-                        cnt[j] -= 1
-                        tatsu.add(Penchan(t))
-                        dfsTatsu(i, 3)
-                        n += 2
-                        cnt[i] += 1
-                        cnt[j] += 1
-                        tatsu.removeLast()
+                    // ryanmen
+                    if (tatsuTypeLimitation <= 2 && t.type != TileType.Z && t.num in 2..7) {
+                        val j = i + 1
+                        if (cnt[i] > 0 && cnt[j] > 0) {
+                            taken = true
+                            n -= 2
+                            cnt[i] -= 1
+                            cnt[j] -= 1
+                            tatsu.add(Ryanmen(t))
+                            dfsTatsu(i, 2)
+                            n += 2
+                            cnt[i] += 1
+                            cnt[j] += 1
+                            tatsu.removeLast()
+                        }
                     }
-                }
 
-                tatsuTypeLimitation = 0
+                    // penchan
+                    if (tatsuTypeLimitation <= 3 && t.type != TileType.Z && (t.num == 1 || t.num == 8)) {
+                        val j = i + 1
+                        if (cnt[i] > 0 && cnt[j] > 0) {
+                            taken = true
+                            n -= 2
+                            cnt[i] -= 1
+                            cnt[j] -= 1
+                            tatsu.add(Penchan(t))
+                            dfsTatsu(i, 3)
+                            n += 2
+                            cnt[i] += 1
+                            cnt[j] += 1
+                            tatsu.removeLast()
+                        }
+                    }
+
+                    tatsuTypeLimitation = 0
+                }
             }
         }
 
-        if (!taken)
+        if (!taken) {
             onResult()
+        }
     }
 
     private fun onResult() {
-        // 将搜索结果处理为（雀头，面子，搭子，浮牌）的形式，且面子数+搭子数不超过k
+        // 将搜索结果处理为（雀头，面子，搭子，浮牌）的形式
         val remaining = buildList {
             for (i in 0 until 3 * 9 + 7) {
                 if (cnt[i] > 0) {
@@ -200,77 +209,15 @@ private class RegularHandPatternSearcher(
             }
         }
 
-        var hasToitsu = false
-
-        tatsu.forEachIndexed { i, tt ->
-            if (tt is Toitsu) {
-                hasToitsu = true
-                val remainingTatsu = tatsu.slice(0 until i) + tatsu.slice(i + 1 until tatsu.size)
-                for ((tatsuChosen, tatsuNotChosenAsTiles) in chooseTatsu(k - mentsu.size, remainingTatsu)) {
-                    val pattern = RegularHandPattern(
-                        k = k + furo.size,
-                        jyantou = tt.first,
-                        menzenMentsu = ArrayList(mentsu),
-                        furo = furo,
-                        tatsu = tatsuChosen,
-                        remaining = remaining + tatsuNotChosenAsTiles
-                    )
-                    callback(pattern)
-                }
-            }
-        }
-
-        if (!hasToitsu) {
-            for ((tatsuChosen, tatsuNotChosenAsTiles) in chooseTatsu(k - mentsu.size, tatsu)) {
-                val pattern = RegularHandPattern(
-                    k = k + furo.size,
-                    jyantou = null,
-                    menzenMentsu = ArrayList(mentsu),
-                    furo = furo,
-                    tatsu = tatsuChosen,
-                    remaining = remaining + tatsuNotChosenAsTiles
-                )
-                callback(pattern)
-            }
-        }
-    }
-
-    private fun chooseTatsu(k: Int, tatsu: List<Tatsu>): List<Pair<List<Tatsu>, List<Tile>>> {
-        return buildList {
-            if (k >= tatsu.size) {
-                add(Pair(ArrayList(tatsu), emptyList()))
-            } else if (k == 0) {
-                add(Pair(emptyList(), buildList {
-                    for (tt in tatsu) {
-                        add(tt.first)
-                        add(tt.second)
-                    }
-                }))
-            } else {
-                val maximum = 1 shl tatsu.size
-                generateKBitNumber(k) { mask ->
-                    if (mask >= maximum) {
-                        false
-                    } else {
-                        val tatsuChosen = ArrayList<Tatsu>()
-                        val tatsuNotChosenAsTiles = ArrayList<Tile>()
-
-                        for (i in tatsu.indices) {
-                            if (mask and (1 shl i) != 0) {
-                                tatsuChosen.add(tatsu[i])
-                            } else {
-                                tatsuNotChosenAsTiles.add(tatsu[i].first)
-                                tatsuNotChosenAsTiles.add(tatsu[i].second)
-                            }
-                        }
-
-                        add(Pair(tatsuChosen, tatsuNotChosenAsTiles))
-
-                        true
-                    }
-                }
-            }
-        }
+        val pattern = RegularHandPattern(
+            k = k + furo.size,
+            jyantou = jyantou,
+            menzenMentsu = ArrayList(mentsu),
+            furo = ArrayList(furo),
+            tatsu = ArrayList(tatsu),
+            remaining = remaining
+        )
+        callback(pattern)
     }
 }
 
