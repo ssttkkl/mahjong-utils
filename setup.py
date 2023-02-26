3042afac0f8a1925cc07ffeffb6af17f970b2c8b
ssttkkl <huang.wen.long@hotmail.com>
2023-02-26 22:05:22 +0800

    Version 0.4.3

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="mahjong-utils",
-    version="0.4.1",
+    version="0.4.3",
     author="ssttkkl",
     author_email="huang.wen.long@hotmail.com",
     license="MIT",
