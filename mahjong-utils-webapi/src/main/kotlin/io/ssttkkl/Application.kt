79fd33d081b967216b13f8dcd9ba3ca6318fc928
ssttkkl <huang.wen.long@hotmail.com>
2023-11-27 15:39:14 +0800

    端口支持通过环境变量配置

--- mahjong-utils-webapi/src/main/kotlin/io/ssttkkl/Application.kt
+++ mahjong-utils-webapi/src/main/kotlin/io/ssttkkl/Application.kt
@@ -12,7 +12,8 @@ import mahjongutils.entry.MethodExecutionException
 import mahjongutils.entry.callReceivingData
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
+    embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
