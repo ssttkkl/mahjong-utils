822e2004e0dd8a945a01f323e87b95ad08bbb777
huangwenlong06 <huangwenlong06@meituan.com>
2023-11-29 14:15:05 +0800

    版本号0.6.1

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="mahjong-utils",
-    version="0.6.0",
+    version="0.6.1",
     author="ssttkkl",
     author_email="huang.wen.long@hotmail.com",
     license="MIT",
