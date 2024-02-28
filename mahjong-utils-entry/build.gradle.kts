729bcaa3cc990cbf82b23bb68eb2d399aa01febf
ssttkkl <huang.wen.long@hotmail.com>
2024-02-28 21:41:05 +0800

    配置编译任务

--- mahjong-utils-entry/build.gradle.kts
+++ mahjong-utils-entry/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithHostTests
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     alias(libs.plugins.buildlogic.kmplib)
@@ -17,39 +17,38 @@ kotlin {
             useCommonJs()
         }
     }
-    wasmJs {
-        browser {
-            binaries.library()
-            useCommonJs()
-        }
-        nodejs {
-            binaries.library()
-            useCommonJs()
-        }
-        applyBinaryen()
-        compilations["main"].packageJson {
-            name = "mahjong-utils-entry-wasm"
-            customField(
-                "author", mapOf(
-                    "name" to "ssttkkl",
-                    "email" to "huang.wen.long@hotmail.com"
-                )
-            )
-            customField(
-                "license", "MIT"
-            )
-        }
-    }
+//    wasmJs {
+//        browser {
+//            binaries.library()
+//            useCommonJs()
+//        }
+//        nodejs {
+//            binaries.library()
+//            useCommonJs()
+//        }
+//        applyBinaryen()
+//        compilations["main"].packageJson {
+//            name = "mahjong-utils-entry-wasm"
+//            customField(
+//                "author", mapOf(
+//                    "name" to "ssttkkl",
+//                    "email" to "huang.wen.long@hotmail.com"
+//                )
+//            )
+//            customField(
+//                "license", "MIT"
+//            )
+//        }
+//    }
 
-    afterEvaluate {
-        tasks.getByName("assembleWasmJsPackage").dependsOn("compileProductionLibraryKotlinWasmJsOptimize")
-    }
+//    afterEvaluate {
+//        tasks.getByName("assembleWasmJsPackage").dependsOn("compileProductionLibraryKotlinWasmJsOptimize")
+//    }
 
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = targets.filterIsInstance<KotlinNativeTargetWithHostTests>().firstOrNull()
-    nativeTarget?.binaries {
-        sharedLib {
+    targets.withType<KotlinNativeTarget>().configureEach {
+        binaries.sharedLib {
             baseName = if (isMingwX64) "libmahjongutils" else "mahjongutils"
         }
     }
@@ -128,5 +127,37 @@ publishing {
             name.set("Mahjong Utils Entry")
             description.set("Provider a union entry for Mahjong Utils (for Japanese Riichi Mahjong)")
         }
+    }
+}
+
+afterEvaluate {
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val isAarch64 = System.getProperty("os.arch") == "aarch64"
+    val currentOsTargetName = when {
+        hostOs == "Mac OS X" -> {
+            if (isAarch64) {
+                "MacosArm64"
+            } else {
+                "MacosX64"
+            }
+        }
+
+        hostOs == "Linux" -> {
+            if (isAarch64) {
+                "LinuxArm64"
+            } else {
+                "LinuxX64"
+            }
+        }
+
+        isMingwX64 -> "MingwX64"
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+    tasks.create("linkDebugSharedForCurrentOs").apply {
+        dependsOn("linkDebugShared" + currentOsTargetName)
+    }
+    tasks.create("linkReleaseSharedForCurrentOs").apply {
+        dependsOn("linkReleaseShared" + currentOsTargetName)
     }
 }
