1ab2dfe6487e1a445934ef11b8604f09142cac58
ssttkkl <ssttkkl@users.noreply.github.com>
2025-02-27 04:41:59 +0000

    New version: 0.7.7

--- python-lib/setup.py
+++ python-lib/setup.py
@@ -132,7 +132,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="mahjong-utils",
-    version="0.7.6",
+    version="0.7.7",
     author="ssttkkl",
     author_email="huang.wen.long@hotmail.com",
     license="MIT",
