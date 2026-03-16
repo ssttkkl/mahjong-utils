d94b0e8880547cd629dd69e5dc0482e9b47bc173
ssttkkl <huang.wen.long@hotmail.com>
2026-03-16 16:48:29 +0800

    Simplify CLI build config using kmpexe plugin
    - Replace kmplib with kmpexe plugin
    - Remove redundant JVM and Native configuration
    - Plugin handles executable setup automatically

--- mahjong-utils-cli/build.gradle.kts
+++ mahjong-utils-cli/build.gradle.kts
@@ -1,39 +1,9 @@
-import mahjongutils.buildlogic.enableNative
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-
 plugins {
-    alias(libs.plugins.buildlogic.kmplib)
+    alias(libs.plugins.buildlogic.kmpexe)
     alias(libs.plugins.kotlin.serialization)
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-
 kotlin {
-    jvm {
-        withJava()
-    }
-
-    if (enableNative) {
-        macosArm64()
-        macosX64()
-        linuxX64()
-        linuxArm64()
-        mingwX64()
-        
-        targets.withType<KotlinNativeTarget>().configureEach {
-            binaries {
-                executable {
-                    entryPoint = "mahjongutils.cli.main"
-                    baseName = "mahjong-utils-cli"
-                }
-            }
-        }
-    }
-
     sourceSets {
         val commonMain by getting {
             dependencies {
